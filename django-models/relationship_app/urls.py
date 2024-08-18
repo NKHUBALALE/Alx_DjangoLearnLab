044d245005e82c12e79be639c8bfa7014272b51a
Nkhubalale <physimanuel@gmail.com>
2024-08-19 01:36:42 +0200

    url config for task1

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView, register_view
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', list_books, name='list_books'),  # Function-based view
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
 
     # Authentication URLs
-    path('register/', views.register_view, name='register'),
+    path('register/', register_view, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
