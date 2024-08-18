56ab37848c2fbf14202033efbbbc97bcf1c16ab8
Nkhubalale <physimanuel@gmail.com>
2024-08-19 00:35:31 +0200

    Revert "task 2"
    This reverts commit 057ddb3bd5e2aeef0cd5c6381054b69f0312d6c0.

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,7 +1,4 @@
-# models.py
-
 from django.db import models
-from django.contrib.auth.models import User
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -29,15 +26,3 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
-
-class UserProfile(models.Model):
-    ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    ]
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
-
-    def __str__(self):
-        return f'{self.user.username} - {self.role}'
--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,16 +1,7 @@
-# urls.py
-
 from django.urls import path
-from .views import list_books, LibraryDetailView, user_login, user_logout, user_register
-from .views import admin_view, librarian_view, member_view
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
-    path('register/', user_register, name='register'),
-    path('admin/', admin_view, name='admin_view'),
-    path('librarian/', librarian_view, name='librarian_view'),
-    path('member/', member_view, name='member_view'),
 ]
