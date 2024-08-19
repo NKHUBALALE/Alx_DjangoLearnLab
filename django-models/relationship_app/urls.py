bdd50250e756ff8694a95115dd7280f0ba63fe84
Nkhubalale <physimanuel@gmail.com>
2024-08-19 02:25:17 +0200

    list books

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . import views
+from . import views 
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books
 
 urlpatterns = [
     # Function-based view for listing books
-    path('books/', views.list_books, name='list_books'),  
+    path('books/', list_books, name='list_books'),  
     
     # Class-based view for library details
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
