ce930825419f2ebfb2aefac079f3630337e16dd4
Nkhubalale <physimanuel@gmail.com>
2024-08-19 15:35:58 +0200

    Revert "task3"
    This reverts commit 5a15539dd52f7958810dfa491c4c1c3da786de0a.

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -14,9 +14,4 @@ urlpatterns = [
 
     # Class-based view for library details
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-
-
-    path('admin/', views.admin_view, name='admin_view'),
-    path('librarian/', views.librarian_view, name='librarian_view'),
-    path('member/', views.member_view, name='member_view'),
 ]
