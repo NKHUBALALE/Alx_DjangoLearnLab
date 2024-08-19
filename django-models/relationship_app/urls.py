29b834df58e3e2c1efbf5d2f23352632908968b7
Nkhubalale <physimanuel@gmail.com>
2024-08-19 02:28:50 +0200

    urls code readability

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-from . import views 
 from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 from .views import list_books
 
 urlpatterns = [
-    # Function-based view for listing books
-    path('books/', list_books, name='list_books'),  
-    
-    # Class-based view for library details
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-
     # Authentication URLs
     path('register/', views.register_view, name='register'),  # Explicitly use views.register_view
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    # Function-based view for listing books
+    path('books/', list_books, name='list_books'),
+
+    # Class-based view for library details
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
