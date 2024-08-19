3a6b75038018a726cb174f1ecd4c2b60b968e0f3
Nkhubalale <physimanuel@gmail.com>
2024-08-19 02:21:03 +0200

    urls configure

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, views  
+from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     # Function-based view for listing books
-    path('books/', list_books, name='list_books'),  
+    path('books/', views.list_books, name='list_books'),  
     
     # Class-based view for library details
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     # Authentication URLs
     path('register/', views.register_view, name='register'),  # Explicitly use views.register_view
