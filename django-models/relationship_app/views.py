5a15539dd52f7958810dfa491c4c1c3da786de0a
Nkhubalale <physimanuel@gmail.com>
2024-08-19 02:46:38 +0200

    task3

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,6 +4,12 @@ from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
 
+
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
+
+
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
@@ -41,3 +47,26 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
+
+
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
