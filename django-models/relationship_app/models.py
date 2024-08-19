fd81d322ce306c983ea93f545f8f27764d6ace3b
Nkhubalale <physimanuel@gmail.com>
2024-08-19 16:16:09 +0200

    updated models to checker if the checker is still happy

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
@@ -14,9 +16,6 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
@@ -25,7 +24,6 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
-
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -36,6 +34,13 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
     def __str__(self):
         return self.title
@@ -53,5 +58,3 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
-
-
