cb9235c58277d96e55a6d265e602e2c5a615f3cc
Nkhubalale <physimanuel@gmail.com>
2024-08-25 22:24:05 +0200

    step 1 again

--- django-models/LibraryProject/bookshelf/models.py
+++ django-models/LibraryProject/bookshelf/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -7,3 +13,5 @@ class Book(models.Model):
     
     def __str__(self):
         return self.title
+    
+
