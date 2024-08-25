b3ac41bde77b789972d38ceef5e329410819a312
Nkhubalale <physimanuel@gmail.com>
2024-08-25 22:47:13 +0200

    step 1 alternative

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
