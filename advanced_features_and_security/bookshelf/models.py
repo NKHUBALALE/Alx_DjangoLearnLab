4c06dcd093c9aecf2f4675875d00cc4ec4511b23
Nkhubalale <physimanuel@gmail.com>
2024-08-25 23:15:18 +0200

    s

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, date_of_birth, password=None, **extra_fields):
@@ -17,15 +18,12 @@ class CustomUserManager(BaseUserManager):
 
         return self.create_user(email, date_of_birth, password, **extra_fields)
 
+
 class CustomUser(AbstractUser):
-    date_of_birth: models.DateField(null=True, blank=True)
-    profile_photo: models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    
     objects = CustomUserManager()
-
-    def __str__(self):
-        return self.email
-
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
