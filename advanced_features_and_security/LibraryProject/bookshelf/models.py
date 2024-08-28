6d49c8d48d73cad76cb55e40043840ba7231f8d5
Nkhubalale <physimanuel@gmail.com>
2024-08-28 04:25:15 +0200

    CustomUserManager

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -21,6 +21,23 @@ class CustomUser(AbstractUser):
         help_text=_("Specific permissions for this user."),
         related_query_name='customuser'
     )
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, date_of_birth=date_of_birth, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, date_of_birth, password, **extra_fields)
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
