b21cf9655f7d8b1d537b19de0f5803bea66e8019
Nkhubalale <physimanuel@gmail.com>
2024-08-30 08:11:55 +0200

    permission decorator

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from django.core.exceptions import PermissionDenied
 from .models import Book
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 
-@login_required
+
+
+@permission_required('bookshelf.can_view_book', raise_exception=True)
 def book_list(request):
-    # Check if the user has the required permission
-    if not request.user.has_perm('bookshelf.can_view_book'):
-        raise PermissionDenied
-    
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
