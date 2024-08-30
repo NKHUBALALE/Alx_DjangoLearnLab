68a4b1a3a2b22c25796d2191d90aa6f09d0998de
Nkhubalale <physimanuel@gmail.com>
2024-08-30 08:09:41 +0200

    errors

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
 from .models import Book
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def book_list(request):
+    # Check if the user has the required permission
+    if not request.user.has_perm('bookshelf.can_view_book'):
+        raise PermissionDenied
+    
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
