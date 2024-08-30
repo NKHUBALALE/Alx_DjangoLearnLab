d553429bae747aa60b5a53adf097c407e3c71d8b
Nkhubalale <physimanuel@gmail.com>
2024-08-30 08:06:09 +0200

    book views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Book
 
-def list_books(request):
+def book_list(request):
     books = Book.objects.all()
-    return render(request, 'bookshelf/list_books.html', {'books': books})
+    return render(request, 'bookshelf/book_list.html', {'books': books})
