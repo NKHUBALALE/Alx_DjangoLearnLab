e66d64945ec155cee5c991b4f00e117fd1ba402a
Nkhubalale <physimanuel@gmail.com>
2024-09-04 16:26:36 +0200

    urls, project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, include
+from django.urls import path
+from .views import BookList
 
 urlpatterns = [
-    # ... other patterns ...
-    path('api/', include('api.urls')),  # Include the api.urls module
+    path('books/', BookList.as_view(), name='book-list'),
 ]
