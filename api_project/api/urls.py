ff0032d751a36a576ba945e68959ef6efc9f00f8
Nkhubalale <physimanuel@gmail.com>
2024-09-04 16:37:38 +0200

    register book view

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+# Create a router and register the BookViewSet
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('api/', include(router.urls))
 ]
