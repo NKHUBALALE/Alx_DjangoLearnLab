7f759c7f34b0de99a4917ee31b6344e2174d3e40
Nkhubalale <physimanuel@gmail.com>
2024-09-04 22:05:40 +0200

    serializers validation

--- advanced-api-project/api/seriealizers.py
+++ advanced-api-project/api/seriealizers.py
@@ -6,6 +6,15 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = '__all__'
 
+    def validate_publication_year(self, value):
+        """
+        Custom validation to ensure publication_year is not in the future.
+        """
+        current_year = datetime.date.today().year
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+
 class AuthorSerializer(serializers.ModelSerializer):
     books = BookSerializer(many=True, read_only=True)
 
