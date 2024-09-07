7b5bb616de46118dcfe2925016e5545b5046ffde
Nkhubalale <physimanuel@gmail.com>
2024-09-07 21:43:07 +0200

    update views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,28 +7,28 @@ from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnl
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]  # Change to IsAuthenticated
+    permission_classes = [IsAuthenticated]
 
 # ListView to retrieve all books
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]  # Allows read-only access for unauthenticated users
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 # DetailView to retrieve a single book by ID
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]  # Allows read-only access for unauthenticated users
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 # UpdateView to update an existing book
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]  # Change to IsAuthenticated
+    permission_classes = [IsAuthenticated]
 
 # DeleteView to delete an existing book
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]  # Change to IsAuthenticated
+    permission_classes = [IsAuthenticated]
