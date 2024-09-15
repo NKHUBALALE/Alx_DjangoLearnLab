dc49e9f16b828d3bc3b4ecff46cc3174fd494c51
Nkhubalale <physimanuel@gmail.com>
2024-09-15 19:33:54 +0200

    blog initial setup

---
+++ django_blog/blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    """
+    A blog post model
+
+    Fields:
+        title (CharField): The title of the post
+        content (TextField): The content of the post
+        published_date (DateTimeField): The date the post was published
+        author (ForeignKey to User): The user who wrote the post
+    """
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
