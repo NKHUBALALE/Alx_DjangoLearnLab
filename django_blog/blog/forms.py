be496254eac752ecddb07dd9f79a5ca98c6969fa
Nkhubalale <physimanuel@gmail.com>
2024-09-17 13:50:12 +0200

    updated models

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,24 +2,38 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post, Comment
+from .models import Post, Comment  # Import your Post and Comment model 
 from taggit.forms import TagField, TagWidget  # Import TagField and TagWidget
 
 class CustomUserForm(UserCreationForm):
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
     email = forms.EmailField(required=True)
 
     class Meta:
+        """
+        The Meta class is used to define the fields to include in the form,
+        and their order.
+        """
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
 class PostForm(forms.ModelForm):
+    """
+    A form that creates or updates a blog post.
+    """
     tags = TagField(required=False, widget=TagWidget())  # Add tags field using TagField and TagWidget
 
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']  # Include tags field
+        fields = ['title', 'content', 'tags']  # Include fields for title, content, and tags
 
 class CommentForm(forms.ModelForm):
+    """
+    A form that creates a comment for a blog post.
+    """
     class Meta:
         model = Comment
-        fields = ['content']
+        fields = ['content']  # Only allow users to enter content for comments
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,66 +1,25 @@
 # blog/models.py
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager  # Import TaggableManager
 
 class Tag(models.Model):
-    """
-    A tag model.
-
-    Attributes:
-        name (CharField): The name of the tag.
-    """
-
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        """
-        Return a string representation of the tag.
-
-        The string is formatted as: "<name>"
-
-        :return: A string representation of the tag
-        """
         return self.name
 
 class Post(models.Model):
-    """
-    A blog post model.
-
-    Attributes:
-        title (CharField): The title of the post.
-        content (TextField): The content of the post.
-        published_date (DateTimeField): The date the post was published.
-        author (ForeignKey to User): The user who wrote the post.
-    """
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+    tags = TaggableManager()  # Add TaggableManager for tagging
 
     def __str__(self):
-        """
-        Return a string representation of the post.
-
-        The string is formatted as: "<title>"
-
-        :return: A string representation of the post
-        """
         return self.title
 
-
 class Comment(models.Model):
-    """
-    A comment model.
-
-    Attributes:
-        post (ForeignKey to Post): The post the comment is on.
-        author (ForeignKey to User): The user who wrote the comment.
-        content (TextField): The content of the comment.
-        created_at (DateTimeField): The date the comment was created.
-        updated_at (DateTimeField): The date the comment was last updated.
-    """
-
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
@@ -68,14 +27,4 @@ class Comment(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        """
-        Return a string representation of the comment.
-
-        The string is formatted as: "Comment by <author> on <post title>"
-
-        :return: A string representation of the comment
-        """
         return f'Comment by {self.author.username} on {self.post.title}'
-
-
-
