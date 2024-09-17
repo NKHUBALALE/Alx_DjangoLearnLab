501219ba46a27cf5b1e98236a1b297236383a4ea
Nkhubalale <physimanuel@gmail.com>
2024-09-17 13:48:07 +0200

    taggit implemented in forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,45 +2,24 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post, Comment  # Import your Post and Comment model 
-from django.shortcuts import render
-
-
-from django.db.models import Q
-
-def search_posts(request):
-    query = request.GET.get('q')
-    results = Post.objects.filter(Q(title__icontains=query) | Q(content__icontains=query) | Q(tags__name__icontains=query)).distinct()
-    return render(request, 'blog/search_results.html', {'results': results, 'query': query})
+from .models import Post, Comment
+from taggit.forms import TagField, TagWidget  # Import TagField and TagWidget
 
 class CustomUserForm(UserCreationForm):
-    """
-    A form that creates a user, with no privileges, from the given username and
-    password.
-    """
     email = forms.EmailField(required=True)
 
     class Meta:
-        """
-        The Meta class is used to define the fields to include in the form,
-        and their order.
-        """
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
 class PostForm(forms.ModelForm):
-    """
-    A form that creates or updates a blog post.
-    """
+    tags = TagField(required=False, widget=TagWidget())  # Add tags field using TagField and TagWidget
+
     class Meta:
         model = Post
-        fields = ['title', 'content']  # Include fields for title and content
-
+        fields = ['title', 'content', 'tags']  # Include tags field
 
 class CommentForm(forms.ModelForm):
-    """
-    A form that creates a comment for a blog post.
-    """
     class Meta:
         model = Comment
-        fields = ['content']  # Only allow users to enter content for comments
+        fields = ['content']
